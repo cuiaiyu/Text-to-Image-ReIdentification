b6c0aea6c92380c67ef3773719d223da400c4e23
Aiyu Cui <aiyucui2@vision-sched.cs.illinois.edu>
2020-02-22 01:42:11 -0600

    readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-ha
+# Language-to-vision Re-Identification: A Re-Implementation of Multi-granularity Image-text Alignments 
+- A algorithm to lan
+- This is an implementation of ICCV'19 paper [Improving Description-based Person Re-identification by Multi-granularity Image-text Alignments](https://arxiv.org/abs/1906.09610). Refer to the original paper for details.
+- This is a beta version implementation. Bug could exist.
+- If you find this is useful in your research work, please cite the original paper (and probably star this repo ;-))
+
+## Dataset
+Please download the train and val1 setfrom [WIDER Person Search by Language](https://competitions.codalab.org/competitions/20142) dataset and save it in proper folder.
+
+## Performance
+With bi-GRU as caption encoder and ResNet-50 as image encoder, we got the following results:
+| Model  | R@1  | R@5   | R@10  | 
+|---|---|---|---|
+|MIA (global-global)  |  47.56 | 71.34  | 79.34  | 
+|MIA (global-global + global-part)   | 50.78 | 73.03  | 82.11   |
+| MIA (global-global + global-part + part-part) __(reported)__  | 53.10 | 75.00   | 82.90  | 
+
+for computational reason, we haven't implemented the __global-global + global-part + part-part__ versiono of MIA, which might be released in next updates.
+
+## Train
+run ```sh src/train.sh```
+
+## Inference
+
+## Visualization
+1. Check notebook [src/inference.ipynb](src/inference.ipynb) for interactive retrieval with config set properly
+
+2. 
+
+
+
--- src/train.sh
+++ src/train.sh
@@ -1,27 +1,17 @@
-export CUDA_VISIBLE_DEVICES=0,2
-export CKPT_ROOT=/shared/rsaas/aiyucui2/wider_person/checkpoints/reID/baseline
-export CKPT_EXP='dist_fn_cosine_imgbb_resnet101_capbb_bigru_embed_size_1024_batch_96_lr_0.0002_step_size_10_captype_sent_img_meltlayer_6_np_False_sent_60_cap_10_6_both_fc_mute_layer1fmap'
-#"dist_fn_cosine_imgbb_resnet50_capbb_bigru_embed_size_1024_batch_96_lr_0.0001_captype_sent_img_meltlayer_2_cos_margin_0.2_np_False"
-# dist_fn_cosine_imgbb_resnet18_capbb_bigru_embed_size_512_batch_96_lr_0.0001_captype_sent_img_meltlayer_8_cos_margin_0.2_np_False
-# dist_fn_cosine_imgbb_resnet18_capbb_bigru_embed_size_512_batch_96_lr_0.0001_captype_sent_img_meltlayer_8
-# "sent_60_cap_10_6_both_truc"
-# $CKPT_ROOT/$CKPT_EXP/stage_1_id_last.pt
+export CKPT_ROOT=checkpoints
+
 python train.py \
---data_root "/data/aiyucui2/wider" \
---model_path "/shared/rsaas/aiyucui2/wider_person/checkpoints/reID/baseline" \
---output_path "/shared/rsaas/aiyucui2/wider_person/outputs/reID/baseline" \
+--data_root "/path/to/data" \
+--model_path "checkpoints/reID/baseline" \
+--output_path "outputs/reID/baseline" \
 --embed_size 1024 \
 --batch_size 96 \
---num_gpus 2 \
---img_backbone_opt resnet101 \
---cap_backbone_opt bigru \
 --dist_fn_opt "cosine" \
 --cap_embed_type sent \
 --image_melt_layer 6 \
 --step_size 10 \
---note "sent_60_cap_10_6_both_fc_mute_layer1fmap" \
---load_ckpt_fn $CKPT_ROOT/$CKPT_EXP/stage_1_id_last.pt \
---num_epochs_stage1 0 \
+--load_ckpt_fn "0" \
+--num_epochs_stage1 10 \
 --num_epochs_stage2 20 \
 --weight_decay 1e-5 \
 --sent_token_length 60 \
